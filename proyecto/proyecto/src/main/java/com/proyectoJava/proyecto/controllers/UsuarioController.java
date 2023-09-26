2d43ac3e2988e049a7536acf9bef51d285862a3a
PavsTEC <pablo.venegas@estudiantec.cr>
2023-09-25 19:20:57 -0600

    getUsuarios funcional con DB
    Ejemplo de conexion a base de datos y envio del json al url con informacion de la tabla

--- proyecto/proyecto/src/main/java/com/proyectoJava/proyecto/controllers/UsuarioController.java
+++ proyecto/proyecto/src/main/java/com/proyectoJava/proyecto/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.proyectoJava.proyecto.controllers;
 
+import com.proyectoJava.proyecto.dao.UsuarioDao;
 import com.proyectoJava.proyecto.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,10 @@ import java.util.List;
 
 @RestController
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioDao usuarioDao;
+
     @RequestMapping(value = "usuario/{id}")
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario = new Usuario();
@@ -23,32 +29,7 @@ public class UsuarioController {
 
     @RequestMapping(value = "usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(234L);
-        usuario.setNombre("Juan");
-        usuario.setApellido("Rodriguez");
-        usuario.setTelefono("87289010");
-        usuario.setEmail("juanro30@gmail.com");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(345L);
-        usuario2.setNombre("Maria");
-        usuario2.setApellido("Gonzalez");
-        usuario2.setTelefono("86778909");
-        usuario2.setEmail("marian40@gmail.com");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(567L);
-        usuario3.setNombre("Pablo");
-        usuario3.setApellido("Vargas");
-        usuario3.setTelefono("67892890");
-        usuario3.setEmail("pablo50@gmail.com");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
     @RequestMapping(value = "usuario1")
--- proyecto/proyecto/src/main/java/com/proyectoJava/proyecto/dao/UsuarioDao.java
+++ proyecto/proyecto/src/main/java/com/proyectoJava/proyecto/dao/UsuarioDao.java
@@ -5,6 +5,13 @@ import com.proyectoJava.proyecto.models.Usuario;
 import java.util.List;
 
 public interface UsuarioDao {
-    List<Usuario> getusuarios();
 
+    List<Usuario> getUsuarios();
+
+    void eliminar(Long id);
+
+    void registrar(Usuario usuario);
+
+    Usuario obtenerUsuarioPorCredenciales(Usuario usuario);
 }
+
