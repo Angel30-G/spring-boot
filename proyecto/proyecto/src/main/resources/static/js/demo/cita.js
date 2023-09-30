c32e5a8990611ada93f8d5d6eef247ce4d835680
Angel <gabrielvargas493@gmail.com>
2023-09-29 22:33:50 -0600

    Arreglos minimos

--- proyecto/proyecto/src/main/resources/static/js/demo/cita.js
+++ proyecto/proyecto/src/main/resources/static/js/demo/cita.js
@@ -1,29 +1,30 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   cargarUsuarios();
-  $('#doctores').DataTable();
+  $('#cita').DataTable();
 });
 
 async function cargarUsuarios(){
 
-  const request = await fetch('medico', {
+  const request = await fetch('CitaAgendada', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
   });
-  const usuarios = await request.json();
+  const Cita = await request.json();
 
   let listadoHtml = '';
-  for(let Medico of doctores){
+  for(let CitaAgendada of Cita){
 
-    let medicoHtml = '<tr><td>'
-    +Medico.cedula+'</td><td>'
-    +Medico.contrasena+'</td><td>'
-    +Medico.especialidad+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+    let citaHtml = '<tr><td>'
+    +Cita.id_cita+'</td><td>'
+    +Cita.cedula+'</td><td>'
+    +Cita.id_medico+'</td><td>'
+    +Cita.fecha_hora+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
 
-    listadoHtml =+ medicoHtml
+    listadoHtml =+ citaHtml
   }
 
   document.querySelector('#doctores tbody').outerHTML = listadoHtml;
--- proyecto/proyecto/src/main/resources/static/js/demo/expediente.js
+++ proyecto/proyecto/src/main/resources/static/js/demo/expediente.js
@@ -18,7 +18,7 @@ async function cargarUsuarios(){
   let listadoHtml = '';
   for(let Expediente of expediente){
 
-    let medicoHtml = '<tr><td>'
+    let expedienteHtml = '<tr><td>'
     +Expediente.id_expediente+'</td><td>'
     +Expediente.cedula+'</td><td>'
     +Expediente.fecha+'</td><td>'
@@ -26,7 +26,7 @@ async function cargarUsuarios(){
     +Expediente.procedimiento+'</td><td>'
     +Expediente.medicamentos+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
 
-    listadoHtml =+ medicoHtml
+    listadoHtml =+ expedienteHtml
   }
 
   document.querySelector('#expediente tbody').outerHTML = listadoHtml;
