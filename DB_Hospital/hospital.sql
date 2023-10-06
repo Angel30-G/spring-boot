06e97ef5ec6a93222d4fae1414d7d1e9659b60d9
PavsTEC <pablo.venegas@estudiantec.cr>
2023-10-06 14:18:40 -0600

    base de datos

--- DB_Hospital/hospital.sql
+++ DB_Hospital/hospital.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2023 at 11:54 PM
+-- Generation Time: Oct 06, 2023 at 10:18 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,8 +31,8 @@ CREATE TABLE `cita_agendada` (
   `id_cita` int(11) NOT NULL,
   `cedula` int(11) NOT NULL,
   `id_medico` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` time DEFAULT NULL
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -52,8 +52,8 @@ INSERT INTO `cita_agendada` (`id_cita`, `cedula`, `id_medico`, `fecha`, `hora`) 
 CREATE TABLE `cita_disponible` (
   `id_cita_disponible` int(11) NOT NULL,
   `id_medico` int(11) NOT NULL,
-  `fecha` date DEFAULT NULL,
-  `hora` time DEFAULT NULL
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -62,7 +62,50 @@ CREATE TABLE `cita_disponible` (
 
 INSERT INTO `cita_disponible` (`id_cita_disponible`, `id_medico`, `fecha`, `hora`) VALUES
 (1, 111111111, '2024-09-06', '15:30:00'),
-(2, 111111111, '2023-11-16', '07:00:00');
+(2, 111111111, '2023-11-16', '07:00:00'),
+(3, 456789012, '2023-10-01', '09:30:00'),
+(4, 456789012, '2023-11-01', '14:00:00'),
+(5, 456789012, '2023-12-15', '08:30:00'),
+(6, 456789012, '2024-01-20', '12:30:00'),
+(7, 456789012, '2024-03-05', '16:00:00'),
+(8, 456789012, '2024-04-10', '10:30:00'),
+(9, 456789012, '2024-05-22', '13:00:00'),
+(10, 543210987, '2023-10-12', '11:30:00'),
+(11, 543210987, '2023-11-20', '15:00:00'),
+(12, 543210987, '2023-12-05', '07:30:00'),
+(13, 555444333, '2023-10-20', '10:30:00'),
+(14, 555444333, '2023-11-25', '16:30:00'),
+(15, 555444333, '2023-12-10', '08:30:00'),
+(16, 555444333, '2024-01-15', '13:00:00'),
+(17, 555444333, '2024-03-20', '09:30:00'),
+(18, 567890123, '2023-11-01', '14:00:00'),
+(19, 567890123, '2023-12-10', '16:30:00'),
+(20, 654321098, '2023-11-10', '15:00:00'),
+(21, 654321098, '2023-12-20', '10:30:00'),
+(22, 654321098, '2024-01-05', '14:00:00'),
+(23, 654321098, '2024-03-15', '08:30:00'),
+(24, 654321098, '2024-04-20', '12:30:00'),
+(25, 654321098, '2024-06-05', '16:00:00'),
+(26, 666333222, '2023-11-16', '07:30:00'),
+(27, 666333222, '2023-12-25', '11:00:00'),
+(28, 666333222, '2024-01-10', '14:30:00'),
+(29, 666333222, '2024-03-18', '09:00:00'),
+(30, 765432109, '2023-11-25', '16:00:00'),
+(31, 765432109, '2024-01-05', '09:30:00'),
+(32, 765432109, '2024-02-15', '13:00:00'),
+(33, 765432109, '2024-04-01', '10:30:00'),
+(34, 777888999, '2024-01-15', '11:00:00'),
+(35, 777888999, '2024-02-25', '14:30:00'),
+(36, 777888999, '2024-04-10', '08:00:00'),
+(37, 789012345, '2024-02-25', '12:30:00'),
+(38, 789012345, '2024-04-05', '08:00:00'),
+(39, 789012345, '2024-05-15', '11:30:00'),
+(40, 789012345, '2024-07-01', '15:00:00'),
+(41, 789012345, '2024-08-10', '09:30:00'),
+(42, 789012345, '2024-09-22', '14:00:00'),
+(43, 876543210, '2024-04-14', '08:00:00'),
+(44, 876543210, '2024-05-25', '11:30:00'),
+(45, 888999777, '2024-06-30', '14:30:00');
 
 -- --------------------------------------------------------
 
@@ -80,7 +123,12 @@ CREATE TABLE `especialidad` (
 --
 
 INSERT INTO `especialidad` (`id_especialidad`, `nombre`) VALUES
-(1, 'Medicina General');
+(1, 'Medicina General'),
+(2, 'Cardiologia'),
+(3, 'Dermatologia'),
+(4, 'Oftalmologia'),
+(5, 'Gastroenterologia'),
+(6, 'Neurologia');
 
 -- --------------------------------------------------------
 
@@ -121,7 +169,18 @@ CREATE TABLE `medico` (
 --
 
 INSERT INTO `medico` (`cedula`, `contrasena`, `id_especialidad`) VALUES
-(111111111, '123', 1);
+(111111111, '123', 1),
+(456789012, '123', 1),
+(543210987, '123', 2),
+(555444333, '123', 3),
+(567890123, '123', 4),
+(654321098, '123', 5),
+(666333222, '123', 6),
+(765432109, '123', 1),
+(777888999, '123', 2),
+(789012345, '123', 3),
+(876543210, '123', 4),
+(888999777, '123', 5);
 
 -- --------------------------------------------------------
 
@@ -132,7 +191,7 @@ INSERT INTO `medico` (`cedula`, `contrasena`, `id_especialidad`) VALUES
 CREATE TABLE `paciente` (
   `cedula` int(11) NOT NULL,
   `contrasena` varchar(40) NOT NULL,
-  `medico_cabecera` int(11) DEFAULT NULL
+  `medico_cabecera` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -140,7 +199,19 @@ CREATE TABLE `paciente` (
 --
 
 INSERT INTO `paciente` (`cedula`, `contrasena`, `medico_cabecera`) VALUES
-(117790909, 'Pablo123', NULL);
+(12345678, '123', 789012345),
+(109876543, '123', 456789012),
+(111222333, '123', 456789012),
+(117790909, 'Pablo123', 111111111),
+(210987654, '123', 456789012),
+(222333444, '123', 765432109),
+(234567890, '123', 765432109),
+(321098765, '123', 543210987),
+(333222111, '123', 765432109),
+(333444555, '123', 765432109),
+(345678912, '123', 765432109),
+(432109876, '123', 654321098),
+(444555666, '123', 765432109);
 
 -- --------------------------------------------------------
 
@@ -160,8 +231,34 @@ CREATE TABLE `persona` (
 --
 
 INSERT INTO `persona` (`cedula`, `nombre`, `apellido`, `correo`) VALUES
-(111111111, 'Persona1', 'Apellido1', 'uno@gmail.com'),
-(117790909, 'Pablo', 'Venegas', 'pablo.venegas@estudiantec.cr');
+(12345678, 'Isabel Lopez', 'Diaz', 'isabel.lopez@email.com'),
+(109876543, 'Isabel Lopez', 'Diaz', 'isabel.lopez@email.com'),
+(111111111, 'Juan', 'Mora', 'mora@gmail.com'),
+(111222333, 'Natalia Vargas', 'Diaz', 'natalia.vargas@email.com'),
+(117790909, 'Pablo', 'Venegas', 'pablo.venegas@estudiantec.cr'),
+(210987654, 'Javier Ramirez', 'Perez', 'javier.ramirez@email.com'),
+(222333444, 'Eduardo Rojas', 'Martinez', 'eduardo.rojas@email.com'),
+(234567890, 'Carlos Rodriguez', 'Martinez', 'carlos.rodriguez@email.com'),
+(321098765, 'Sofia Garcia', 'Molina', 'sofia.garcia@email.com'),
+(333222111, 'Marta Rodriguez', 'Lopez', 'marta.rodriguez@email.com'),
+(333444555, 'Carmen Cruz', 'Sanchez', 'carmen.cruz@email.com'),
+(345678912, 'Juan Perez', 'Gomez', 'juan.perez@email.com'),
+(432109876, 'Fernando Perez', 'Gonzalez', 'fernando.perez@email.com'),
+(444555666, 'Carlos Soto', 'Gomez', 'carlos.soto@email.com'),
+(456789012, 'Diego Ramirez', 'Hernandez', 'diego.ramirez@email.com'),
+(543210987, 'Ricardo Mendez', 'Santos', 'ricardo.mendez@email.com'),
+(555444333, 'Lucia Morales', 'Vega', 'lucia.morales@email.com'),
+(567890123, 'Ana Martinez', 'Fernandez', 'ana.martinez@email.com'),
+(654321098, 'Gabriela Torres', 'Lopez', 'gabriela.torres@email.com'),
+(666333222, 'Pablo Ortiz', 'Garcia', 'pablo.ortiz@email.com'),
+(765432109, 'Andres Hernandez', 'Gomez', 'andres.hernandez@email.com'),
+(777888999, 'Valeria Castro', 'Hernandez', 'valeria.castro@email.com'),
+(789012345, 'Laura Lopez', 'Sanchez', 'laura.lopez@email.com'),
+(876543210, 'Maria Gonzalez', 'Lopez', 'maria.gonzalez@email.com'),
+(888999777, 'Felipe Mora', 'Gomez', 'felipe.mora@email.com'),
+(890123456, 'Pedro Sanchez', 'Garcia', 'pedro.sanchez@email.com'),
+(987654321, 'Elena Rios', 'Castro', 'elena.rios@email.com'),
+(999888777, 'Camila Hernandez', 'Lopez', 'camila.hernandez@email.com');
 
 -- --------------------------------------------------------
 
@@ -259,13 +356,13 @@ ALTER TABLE `cita_agendada`
 -- AUTO_INCREMENT for table `cita_disponible`
 --
 ALTER TABLE `cita_disponible`
-  MODIFY `id_cita_disponible` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_cita_disponible` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT for table `especialidad`
 --
 ALTER TABLE `especialidad`
-  MODIFY `id_especialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_especialidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `expediente`
